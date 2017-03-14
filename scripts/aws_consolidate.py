b773d36cb61d374c57230ccb8c6e5f9e4be27772
mgzeitouni <mgzeitou@us.ibm.com>
2017-03-14 14:25:17 +0100

    finished weekly consolidation process

--- scripts/aws_consolidate.py
+++ scripts/aws_consolidate.py
@@ -6,13 +6,16 @@ import botocore
 import pandas as pd
 import smart_open
 import csv
+import requests
+import shutil
 
 s3_resource = boto3.resource('s3')
 
-def add_to_total(day):
+def consolidate_dailys(day):
 
-	
 	bucket = s3_resource.Bucket('2017pricedata')
+
+	total_size = 0
 
 	for obj in bucket.objects.all():
 		
@@ -21,8 +24,8 @@ def add_to_total(day):
 		if 'daily' in key and 'csv' in key:
 
 			key_list = key.split('/')
-			pdb.set_trace()
-			print key_list
+		#	pdb.set_trace()
+
 			file_day = key_list[1]
 			team = key_list[2]
 			event_csv = key_list[3]
@@ -30,39 +33,98 @@ def add_to_total(day):
 			if file_day == day:
 
 				lines = []
-
+				#print 's3://2017pricedata/daily/%s/%s/%s' %(day,team,event_csv)
+				first_line = True
 				for line in smart_open.smart_open('s3://2017pricedata/daily/%s/%s/%s' %(day,team,event_csv)):
 					
+					if not first_line:
+						lines.append(line.replace('\r\n','').split(','))
+					else:
+						first_line= False
 
-					lines.append(line.replace('\r\n','').split(','))
+				total_size += append_to_total(event_csv, lines, team)
 
-					append_to_total(event_csv, lines, team, day)
+	return total_size
 
 
-def append_to_total(event_csv, lines, team, day):
-
-	lines = []
+def append_to_total(event_csv, lines, team):
 
 	bucket = s3_resource.Bucket('2017pricedata')
 
+	import boto.s3.connection
+	access_key = 'AKIAIOWD5SFYZNZTNPWA'
+	secret_key = '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'
 
-	for line in smart_open.smart_open('s3://2017pricedata/total/%s/%s' %(day,team,event_csv)):
+	conn = boto.connect_s3(
+        aws_access_key_id = access_key,
+        aws_secret_access_key = secret_key,
+      #  host = 'objects.dreamhost.com',
+        #is_secure=False,               # uncomment if you are not using ssl
+        calling_format = boto.s3.connection.OrdinaryCallingFormat(),
+        )
+
+	bucket = conn.get_bucket('2017pricedata')
+
+	key = 'total/%s/%s' %(team,event_csv)
+	exists = False
+	existing_lines = []
+	key_file = None
+
+
+	for obj in bucket.list():
 		
+		if obj.key == key:
 
-		lines.append(line.replace('\r\n','').split(','))
+			exists = True
+
+			# Grab current 'total data'
+			for x in smart_open.smart_open('s3://2017pricedata/total/%s/%s' %(team,event_csv)):
+
+				existing_lines.append(x.replace('\r\n','').split(','))
+
+			# Append the newest data from this day
+
+			for row in lines:
+
+				existing_lines.append(row)
+
+			key_file = obj
+
+			break
+
+		elif 'total' in obj.key:
+
+			key_file = bucket.new_key(key)
+
+			existing_lines = list(lines)
+
+			break
+
+		else:
+			print 'not our total file - %s' %obj.key
 
 
-    # for root,dirs,files in os.walk(path):
 
-    # 	for team in dirs:
+	# Save to tmp csv
+	directory = '../price_data/tmp/%s' %(team)
 
-    # 		for team_root, team_dirs, team_files in os.walk('%s/%s' %(path,team)):
-    
-    # 			for file in team_files:
-    		
-    # 				local_path = '%s/%s/%s' %(path,team, file)
-    				
-		  #       	s3.upload_file(local_path,bucketname,'daily/%s/%s/%s' %(yesterday_dir,team, file))
+	tmp_file_name = '%s/%s' %(directory,event_csv)
+
+	os.makedirs(directory)
+
+	with open (tmp_file_name, 'wb') as new_file:
+
+		writer = csv.writer(new_file)
+
+		writer.writerows(existing_lines)
+
+	size = os.path.getsize(tmp_file_name)/1000
+
+	key_file.set_contents_from_filename(tmp_file_name)
+
+	shutil.rmtree(directory) 
+
+	return size
 
 
 if __name__ == '__main__':
@@ -76,16 +138,43 @@ if __name__ == '__main__':
 		day = now - datetime.timedelta(days=i)
 		days_to_collect.append('%s_%s_%s' %(day.year, day.month, day.day))
 
-	for day in days_to_collect:
+	sizes = {}
+	total_size = 0
+	days_consolidated = []
+	collection_file_path = '../weekly_consolidation_log.csv'
 
-		add_to_total(day)
+	with open(collection_file_path, 'rU') as log_file:
 
-	# yesterday_dir  = '%s_%s_%s' %(yesterday.year, yesterday.month, yesterday.day)
+		reader = csv.reader(log_file)
+		reader.next()
+		
 
-	# path_to_upload = '../price_data/%s' %yesterday_dir
+		for row in reader:
 
-	# Check if this path exists, and upload it
+			days_list = eval(row[1])
 
-	# if os.path.exists(path_to_upload):
+			for day in days_list:
+				days_consolidated.append(day)
 
-	# 	uploadDirectory(path_to_upload, '2017pricedata', yesterday_dir)
+	for day in reversed(days_to_collect):
+
+		print day
+
+		if day not in days_consolidated:
+			
+			size = consolidate_dailys(day)
+
+			total_size+=size
+
+			sizes[day] = size
+
+			print sizes[day]
+
+
+	with open(collection_file_path, 'a') as log_file:
+
+		writer = csv.writer(log_file)
+
+		writer.writerow([now, days_to_collect,total_size,sizes ])
+
+
