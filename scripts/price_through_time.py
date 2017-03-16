947a77cb7183e885bd7f7deed94246a06a388510
mgzeitouni <mgzeitou@us.ibm.com>
2017-03-16 21:41:41 +0100

    commented stuff

--- scripts/price_through_time.py
+++ scripts/price_through_time.py
@@ -12,23 +12,23 @@ latest_game_schedules = '2017-03-05_18_29'
 # 10 UTC = 5 PM EST, 4 PM PT, 11 PM CET
 # 22 UTC = 5 AM EST, 5 AM PT, 11 AM CET
 
-status_times = {1: [10,22],  # Change to 2, 14
-                2: [10,16,22,4],
-                3: [10,13,16,19,22,1,4,7],
-                4: [11,12,13,14,15,16,17,18,19,20,21,22,23,1,2,3,4,5,6,7,8,9,10]
-}
+# status_times = {1: [10,22],  # Change to 2, 14
+#                 2: [10,16,22,4],
+#                 3: [10,13,16,19,22,1,4,7],
+#                 4: [11,12,13,14,15,16,17,18,19,20,21,22,23,1,2,3,4,5,6,7,8,9,10]
+# }
 
-def get_status(event_id):
+# def get_status(event_id):
     
-    start = datetime.datetime.utcnow()
+#     start = datetime.datetime.utcnow()
 
-    events=[]
-    status = {}
-    dates = {}
-    teams = {}
-    sports = {}
+#     events=[]
+#     status = {}
+#     dates = {}
+#     teams = {}
+#     sports = {}
 
-    now = datetime.datetime.utcnow()
+#     now = datetime.datetime.utcnow()
 
     # with open('../global_data/game_schedules/%s.csv' %latest_game_schedules, 'rU') as schedule_file:
         
@@ -220,10 +220,13 @@ if __name__ == '__main__':
         event_id = sys.argv[2]
         sport = sys.argv[3]
 
- 
         update_event_data(event, team, sport)
+
+        print 'success'
 
     except Exception as e:
 
-        logging.error(traceback.format_exc())
+        print 'problem'
+
+        #logging.error(traceback.format_exc())
 
