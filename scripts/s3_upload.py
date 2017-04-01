b0c9ff297c455bb3f60eda49655d79ee263aea86
mgzeitouni <mgzeitou@us.ibm.com>
2017-04-01 19:52:49 +0200

    s3 upload

--- scripts/s3_upload.py
+++ scripts/s3_upload.py
@@ -2,7 +2,39 @@ import boto3
 import os
 import datetime
 import pdb
+from cloudant.client import Cloudant
+
+
 s3 = boto3.client('s3')
+
+teams = {}
+
+def cloudant_write(new_data):
+
+	if 'VCAP_SERVICES' in os.environ:
+
+    vcap = json.loads(os.getenv('VCAP_SERVICES'))
+    print('Found VCAP_SERVICES')
+    if 'cloudantNoSQLDB' in vcap:
+        creds = vcap['cloudantNoSQLDB'][0]['credentials']
+        user = creds['username']
+        password = creds['password']
+        url = 'https://' + creds['host']
+        client = Cloudant(user, password, url=url, connect=True)
+  
+	else:
+
+		client = Cloudant(CLOUDANT['username'], CLOUDANT['password'], url=CLOUDANT['url'],connect=True,auto_renew=True)
+	
+
+	db = client['data_collection']
+	logs_doc = db["daily_upload"]
+
+	logs = logs_doc["logs"]
+
+	logs.append(new_data)
+
+	logs_doc.save()
 
 
 def uploadDirectory(path,bucketname, yesterday_dir):
@@ -18,9 +50,11 @@ def uploadDirectory(path,bucketname, yesterday_dir):
     				
 		        	s3.upload_file(local_path,bucketname,'daily/%s/%s/%s' %(yesterday_dir,team, file))
 
-def upload_Collection_Logs():
+		        	if team not in teams.keys():
+		        		teams[team] =[]
+		        		
+		        	teams[team].push(file)
 
-	s3.upload_file('../Collection_Logs.csv','2017pricedata','Collection_Logs.csv')
 
 
 if __name__ == '__main__':
@@ -39,4 +73,16 @@ if __name__ == '__main__':
 
 		uploadDirectory(path_to_upload, '2017pricedata', yesterday_dir)
 
-	upload_Collection_Logs()
+
+
+		new_data ={}
+		new_data[yesterday_dir]={}
+
+		for team in teams:
+
+			new_data[yesterday_dir][team] = teams[team]
+
+		cloudant_write(new_data)
+		
+
+
