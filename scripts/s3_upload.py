8c57917b94b703ea1e4c4aa22bf67ae5db1fa5cc
mgzeitouni <mgzeitou@us.ibm.com>
2017-04-03 21:56:09 +0200

    cloudant writes for upload

--- scripts/s3_upload.py
+++ scripts/s3_upload.py
@@ -6,36 +6,83 @@ from cloudant.client import Cloudant
 import smart_open
 import csv
 import shutil
+from credentials import *
 
 
 s3 = boto3.client('s3')
 
 teams = {}
 
-def cloudant_write(new_data):
+if 'VCAP_SERVICES' in os.environ:
+	vcap = json.loads(os.getenv('VCAP_SERVICES'))
 
-	if 'VCAP_SERVICES' in os.environ:
+	print 'Found VCAP_SERVICES'
 
-	    vcap = json.loads(os.getenv('VCAP_SERVICES'))
-	    print 'Found VCAP_SERVICES'
 	if 'cloudantNoSQLDB' in vcap:
 		creds = vcap['cloudantNoSQLDB'][0]['credentials']
 		user = creds['username']
 		password = creds['password']
 		url = 'https://' + creds['host']
 		client = Cloudant(user, password, url=url, connect=True)
-	else:
-		client = Cloudant(CLOUDANT['username'], CLOUDANT['password'], url=CLOUDANT['url'],connect=True,auto_renew=True)
-	
+else:
+	client = Cloudant(CLOUDANT['username'], CLOUDANT['password'], url=CLOUDANT['url'],connect=True,auto_renew=True)
 
-	db = client['data_collection']
-	logs_doc = db["daily_upload"]
+db = client['data_collection']
+
+def cloudant_write_daily(new_data):
+
+
+	current_week =  datetime.datetime.utcnow().isocalendar()[1]
+	current_year = datetime.datetime.utcnow().year
+
+	doc_id = "daily_upload_%s_week_%s" %(current_year,current_week)
+	exists = False
+
+	for doc in db:
+		if doc['_id'] == doc_id:
+			exists = True
+			break
+	
+	if exists == False:
+
+		db.create_document({"_id":"daily_upload_%s_week_%s" %(current_year,current_week),
+							"logs":[]})
+	
+	logs_doc = db[doc_id]
 
 	logs = logs_doc["logs"]
 
 	logs.append(new_data)
 
 	logs_doc.save()
+
+def cloudant_write_total(new_data):
+
+	current_week =  datetime.datetime.utcnow().isocalendar()[1]
+	current_year = datetime.datetime.utcnow().year
+
+	doc_id = "total_upload_%s_week_%s" %(current_year,current_week)
+
+	exists = False
+	for doc in db:
+		if doc['_id'] == doc_id:
+			exists = True
+			break
+
+
+	if exists == False:
+	
+		db.create_document({"_id":"total_upload_%s_week_%s" %(current_year,current_week),
+							"logs":[]})
+	
+	logs_doc = db["total_upload_%s_week_%s" %(current_year,current_week)]
+
+	logs = logs_doc["logs"]
+
+	logs.append(new_data)
+
+	logs_doc.save()
+
 
 
 def uploadDirectory(path,bucketname, yesterday_dir):
@@ -52,7 +99,21 @@ def uploadDirectory(path,bucketname, yesterday_dir):
 
     				s3.upload_file(local_path,bucketname,'daily/%s/%s/%s' %(yesterday_dir,team, file))
 
+    				day_size = os.path.getsize(local_path)/1000
+
+    				new_data = {
+
+    						"UTC_Timestamp": str(datetime.datetime.utcnow()),
+    						"Team_Uploaded": team,
+    						"Event":file.replace('.csv',''),
+    						"Day_Collected":yesterday_dir,
+    						"Size_KB": day_size
+
+    				}
+
+    				cloudant_write_daily(new_data)
     				existing_lines = []
+    				
 
 		        	# Get new lines for daily
 
@@ -61,9 +122,6 @@ def uploadDirectory(path,bucketname, yesterday_dir):
 		        		reader = csv.reader(new_data)
 		        		reader.next()
 		        		new_rows = [l for l in reader]
-
-
-		        	
 
 
 		        	directory = '../price_data/tmp/%s' %(team)
@@ -81,13 +139,6 @@ def uploadDirectory(path,bucketname, yesterday_dir):
 						with open (tmp_file_name, 'a') as new_file:
 
 							writer = csv.writer(new_file)
-							
-
-							# header = ['Time','Time_Diff','Zone_Section_Id','Zone_Name','Total_Tickets','Average_Price','Zone_Section_Total_Tickets','Zone_Section_Average_Price','Zone_Section_Min_Price','Zone_Section_Max_Price','Zone_Section_Std','Win_PCT','Total_Games','L_10','Section_Median','Total_Listings','Zone_Section_Num_Listings', 'Data_Type', 'Event_Id']
-
-							# writer.writerow(header)
-
-							# writer.writerows(existing_lines)
 
 							writer.writerows(new_rows)
 						
@@ -97,6 +148,8 @@ def uploadDirectory(path,bucketname, yesterday_dir):
 						s3_client.upload_file(tmp_file_name, '2017pricedata','total/%s/%s' %(team,file))
 
 						shutil.rmtree(directory)
+
+						new_doc = False
 
 
 		        	except:
@@ -112,15 +165,27 @@ def uploadDirectory(path,bucketname, yesterday_dir):
 							writer.writerows(new_rows)
 
 		        		print 'doesnt exist -starting new'
-		        		pdb.set_trace()
+		        		
 		        		s3.upload_file(local_path,bucketname,'total/%s/%s' %(team, file))
+		        		size = os.path.getsize(tmp_file_name)/1000
 		        		shutil.rmtree(directory) 
 
+		        		new_doc =True
 
-	        	# if team not in teams.keys():
-	        	# 	teams[team] =[]
-	        		
-	        	# teams[team].append(file)
+
+		        	new_data = {
+
+    						"UTC_Timestamp": str(datetime.datetime.utcnow()),
+    						"Team_Uploaded": team,
+    						"Event":file.replace('.csv',''),
+    						"Day_Added_To_Total":yesterday_dir,
+    						"Size_KB": size,
+    						"New_doc":new_doc
+
+    					}
+
+    				cloudant_write_total(new_data)
+    				pdb.set_trace()
 
 
 
